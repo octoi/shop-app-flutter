691bfe6d59196c3807167242e91f302b87174116
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-15 12:24:50 +0530

    order screen & widget

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart' as ord;
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+
+  const OrderItem(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${order.amount}'),
+            subtitle: Text(
+              DateFormat('dd MM YYYY hh:mm').format(order.dateTime),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.expand_more),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
