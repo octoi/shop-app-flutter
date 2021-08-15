e3105dd4f6a0c5fa264e03acd458c97e903b8d59
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-15 16:50:27 +0530

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/orders.dart';
 import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/screens/edit_product_screen.dart';
 import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (_) => CartScreen(),
           OrdersScreen.routeName: (_) => OrdersScreen(),
           UserProductsScreen.routeName: (_) => UserProductsScreen(),
+          EditProductsScreen.routeName: (_) => EditProductsScreen(),
         },
       ),
     );
---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class EditProductsScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
+  const EditProductsScreen({Key? key}) : super(key: key);
+
+  @override
+  _EditProductsScreenState createState() => _EditProductsScreenState();
+}
+
+class _EditProductsScreenState extends State<EditProductsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
