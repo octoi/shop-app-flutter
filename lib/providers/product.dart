61b393f5ce90de3ed3726c10ada305af49983e9d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-16 21:36:06 +0530

    fav

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop_app/config.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +21,28 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newVal) {
+    isFavorite = newVal;
+    notifyListeners();
+  }
+
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+
+    final url = SERVER_URL + '/products/$id.json';
+
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode({'isFavorite': isFavorite}),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (err) {
+      _setFavValue(oldStatus);
+    }
   }
 }
