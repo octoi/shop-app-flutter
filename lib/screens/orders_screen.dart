76060b2fced9f2d45863eb03e72dd12da5614df9
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-16 22:31:40 +0530

    better way to order

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,51 +4,37 @@ import 'package:shop_app/providers/orders.dart' show Orders;
 import 'package:shop_app/widgets/app_drawer.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
-
-  const OrdersScreen({Key? key}) : super(key: key);
-
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() => _isLoading = true);
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() => _isLoading = false);
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          setState(() => _isLoading = true);
-          await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-          setState(() => _isLoading = false);
-        },
-        child: _isLoading
-            ? Center(child: CircularProgressIndicator())
-            : ListView.builder(
-                itemCount: orderData.orders.length,
-                itemBuilder: (ctx, idx) {
-                  return OrderItem(orderData.orders[idx]);
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              return Center(child: Text('An error occurred !!'));
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, _) {
+                  return ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, idx) {
+                      return OrderItem(orderData.orders[idx]);
+                    },
+                  );
                 },
-              ),
+              );
+            }
+          }
+        },
       ),
     );
   }
